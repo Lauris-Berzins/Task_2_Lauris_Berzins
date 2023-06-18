d19b9cbfe7bb0be59f27841f72a36e7d9b871648
Lauris-Berzins <Lauris.Berzinsh2002@gmail.com>
2023-06-18 19:35:21 +0300

    a lot

--- assets/js/Ideja.js
+++ assets/js/Ideja.js
@@ -1,41 +1,57 @@
 var forward_button = document.querySelector(".forward_button");
 var back_button = document.querySelector(".back_button");
 
-var paragraph1 = document.querySelector("first_quote");
-var paragraph2 = document.querySelector("2_quote");
-var paragraph3 = document.querySelector("3_quote");
-var paragraph4 = document.querySelector("4_quote");
-var paragraph5 = document.querySelector("5_quote");
+var paragraph1 = document.querySelector(".first_quote");
+var paragraph2 = document.querySelector(".second_quote");
+var paragraph3 = document.querySelector(".third_quote");
+var paragraph4 = document.querySelector(".forth_quote");
+var paragraph5 = document.querySelector(".f_quote");
+
+var variable_list = [paragraph1,paragraph2,paragraph3,paragraph4,paragraph5]
 
 let revolving = 0
 
-if (revolving===0){
-    paragraph1.classList.toggle("drag_in");
 
-}else if (revolving==1){
-    paragraph2.classList.toggle("dragin");
+forward_button.addEventListener("click", function() {
+    // Remove "dragin" and "dragout" classes from all paragraphs
+    for (let i = 0; i < variable_list.length; i++) {
+      variable_list[i].classList.remove("dragin", "dragout");
+    }
+  
+    // Add "dragout" class to the current paragraph
+    variable_list[revolving].classList.add("dragout");
+  
+    revolving++;
+    if (revolving >= variable_list.length) {
+      revolving = 0;
+    }
+  
+    // Add "dragin" class to the new paragraph
+    variable_list[revolving].classList.add("dragin");
+  
+    // Update the quote number
+    quoteNumber.textContent = revolving + 1;
+  });
+  
+  back_button.addEventListener("click", function() {
+    // Remove "dragin" and "dragout" classes from all paragraphs
+    for (let i = 0; i < variable_list.length; i++) {
+      variable_list[i].classList.remove("dragin", "dragout");
+    }
+  
+    // Add "dragout" class to the current paragraph
+    variable_list[revolving].classList.add("dragout");
+  
+    revolving--;
+    if (revolving < 0) {
+      revolving = variable_list.length - 1;
+    }
+  
+    // Add "dragin" class to the new paragraph
+    variable_list[revolving].classList.add("dragin");
+  
+    // Update the quote number
+    quoteNumber.textContent = revolving + 1;
+  });
 
-}else if (revolving==2){
-    paragraph3.classList.toggle("dragin");
-
-}else if (revolving==3){
-    paragraph4.classList.toggle("dragin");
-
-}else if (revolving==4){
-    paragraph5.classList.toggle("dragin");
-}
-
-forward_button.addEventListener("click", function(){
-    revolving++
-});
-
-if (revolving !==0){
-    back_button.classList.toggle("gray_out")
-    back_button.addEventListener("click", function(){
-        revolving = revolving-1
-});
-
-}else if(revolving==0){
-    back_button.classList.toggle("gray_in")
-}
 
